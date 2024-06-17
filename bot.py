10261d0fc2cfe3149c69db7d6cf9e76ee9118821
Dillonzer <dillonzerapps@gmail.com>
2024-06-17 01:32:17 -0600

    Templating

---
+++ bot.py
@@ -0,0 +1,6 @@
+import interactions
+from consts import Consts
+import aiohttp
+import asyncio
+
+client = interactions.Client(token=Consts.TOKEN)
---
+++ consts.py
@@ -0,0 +1,5 @@
+from os import environ
+
+class Consts:    
+    BOT_ID = environ.get('BOT_ID')
+    TOKEN = environ.get('TOKEN')
