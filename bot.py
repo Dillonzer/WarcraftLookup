0ac6e499f8146cb5da50830a9138975a4e5edfdf
Dillonzer <dillonzerapps@gmail.com>
2024-06-19 09:25:36 -0600

    Adding presence

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
-from interactions import Client
+from interactions import Client, listen
 from consts import Consts
-import aiohttp
 
 bot = Client(token=Consts.TOKEN)
 bot.load_extension("information")
 bot.load_extension("raiderio")
 bot.load_extension("warcraftlogs")
+
+@listen()
+async def on_startup():
+    await bot.change_presence(activity="World of Warcraft")
 
 bot.start()
